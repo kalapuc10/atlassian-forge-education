d2f909dcf63b67a42eb3c45cff32c8a86734801a
Andrej Jurkevic <andrej.jurkevic@gmail.com>
2022-09-18 19:50:35 +0200

    removed soures

--- src/hooks/use-tasks.js
+++
@@ -1,5 +0,0 @@
-import { useIssueProperty } from '@forge/ui-jira';
-
-const ISSUE_PROPERTY_PREFIX = 'jira-checklist-';
-
-export const useTasks = () => useIssueProperty(ISSUE_PROPERTY_PREFIX + 'tasks', []);
